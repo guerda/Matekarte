2f6398f70a7d72af4fda60ba56c824d847a8a822
Philip Gillißen <guerda@freenet.de>
2014-12-17 17:59:23 +0100

    Added rest example for new dealer
    does not quite work yet, maybe I recorded the POST request wrong

---
+++ matekarte-rest-examples/new_dealer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl --data "dealer[address]=Neustraße 21&dealer[city]=Düsseldorf&dealer[country]=Deutschland&dealer[drink_ids][]=50fb3d16ce007c40fc00080d&dealer[name]=Kiosk & Solarium&dealer[note]=&dealer[phone]=&dealer[type]=1&dealer[www]=&dealer[zip]=40213&utf8=✓" https://www.matekarte.de/dealers/new
