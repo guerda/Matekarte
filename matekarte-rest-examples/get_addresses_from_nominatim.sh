20d9940e0cd248022e85f935c89bbedc1cd157c3
Philip Gillißen <guerda@freenet.de>
2014-12-27 13:41:46 +0100

    Added reverse geocoding with Nominatim (OSM)

---
+++ matekarte-rest-examples/get_addresses_from_nominatim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl "http://nominatim.openstreetmap.org/reverse?lat=51.039577&lon=6.979935&format=json&addressdetails=1&accept-language=de,en&zoom=18"
--- matekarte-rest-examples/get_dealer_info.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl https://www.matekarte.de/api/v2/dealers/50fb3d15ce007c40fc000694
